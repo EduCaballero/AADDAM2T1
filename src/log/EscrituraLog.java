7d5be25ff5e3af00ea08f5c1075ca33441569a80
EduStrife <edustrife@gmail.com>
2017-12-14 20:15:14 +0100

    Comentarios añadidos

--- src/log/EscrituraLog.java
+++ src/log/EscrituraLog.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class EscrituraLog {
 	
 	public void escribir(String lineaEscribir){
-	    String frase=lineaEscribir;
+	    String frase=lineaEscribir; //le decimos aquí lo que vamos a escribir
 	    try {
 	        FileWriter escritura=new FileWriter("incidencies.log", true); //con true hacemos que si el fichero ya existe, le añada la información
 																				//si false, sobrescribiría el fichero entero en lugar de añadir
@@ -16,12 +16,12 @@ public class EscrituraLog {
 	        try{
 	        	miBufer.write(frase);
 	        } catch (IOException ex) {
-	        	System.out.println("No se ha podido escribir en el fichero auxiliar "+ex);//lkjbsdljndsglnjl
-	        	File f1=new File("incidencies.log");
-	        	f1.delete();
+	        	System.out.println("No se ha podido escribir en el fichero auxiliar "+ex);//Mostramos el mensaje de error si no hemos podido escribir
+	        	File f1=new File("incidencies.log"); //guardamos en la variable f1 el fichero incidencies.log para...
+	        	f1.delete();//...borrarlo aquí si ha dado error 
 	        }
-	        miBufer.newLine();
-	        miBufer.flush();
+	        miBufer.newLine(); //salto de línea
+	        miBufer.flush(); //cerramos con flush y close
 	        miBufer.close();
 	        //System.out.println("Fichero incidencies.log escrito");
 	
